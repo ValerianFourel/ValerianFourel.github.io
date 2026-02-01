e4a34e6f035b98a36a8c4ece4d9c83a0207db956
ValerianFourel <77210325+ValerianFourel@users.noreply.github.com>
2026-02-01 14:34:55 +0000

    Deploying to gh-pages from @ ValerianFourel/ValerianFourel.github.io@52ffa7dfd860af46eaf6f01e7f08e3dfadea3b12 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -450,7 +450,7 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("/assets/pdf/ValerianFourelResume_Feb1st2026.pdf", "_blank");
         },
       },{
         id: 'social-email',
