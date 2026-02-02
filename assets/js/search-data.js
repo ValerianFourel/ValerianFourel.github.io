1d94508a4d5c4cdf4fedb004a8da9ef03e4706bd
ValerianFourel <77210325+ValerianFourel@users.noreply.github.com>
2026-02-02 15:05:43 +0000

    Deploying to gh-pages from @ ValerianFourel/ValerianFourel.github.io@ea65f6c4d011ecad75e19f844b411b20b81a6778 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -408,36 +408,16 @@ ninja.data = [{
           description: "A VLM to Label Human Faces?",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+            },},{id: "projects-market-basket-analysis",
+          title: 'Market Basket Analysis',
+          description: "Analyzing seasonal changes in consumer behavior using the Apriori algorithm",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
+            },},{id: "projects-generative-ai-for-sustainability",
+          title: 'Generative AI for Sustainability',
+          description: "Using VAEs fitted on satellite data for soil carbon content estimation",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
             },},{
         id: 'social-cv',
         title: 'CV',
